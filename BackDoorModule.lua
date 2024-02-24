f6d3a56a9eee831cc1416c2080d9891ef679759f
HalalDev <54723649+Halal-Dev@users.noreply.github.com>
2024-02-24 02:50:25 +0100

    Added HookAllFuncs + Special UDPManager hook
    HookAllFuncs allows you to monitor all functions inside a table
    
    Also added a special hook to udpmanager to monitor all network activities

--- BackDoorModule.lua
+++ BackDoorModule.lua
@@ -54,19 +54,20 @@ BackdoorModule.log = function(mode,...)
 end
 
 -- deep dumps the contents of the table and it's contents' contents
-BackdoorModule.deepdump = function( tbl )
+BackdoorModule.deepdump = function(tbl,outputFunc)
+    outputFunc = outputFunc or function(...) BackdoorModule.log("Trace", ...) end
     local checklist = {}
     local function innerdump( tbl, indent )
         checklist[ tostring(tbl) ] = true
         for k,v in pairs(tbl) do
-            Log(indent..k,v,type(v),checklist[ tostring(tbl) ])
+            outputFunc(indent..k,v,type(v),checklist[ tostring(tbl) ])
             if (type(v) == "table" and not checklist[ tostring(v) ]) then innerdump(v,indent.."    ") end
         end
     end
-    Log("====== DEEP DUMP "..tostring(tbl).." =======")
+    outputFunc("====== DEEP DUMP "..tostring(tbl).." =======")
     checklist[ tostring(tbl) ] = true
     innerdump( tbl, "" )
-    Log("-----------------------------------")
+    outputFunc("-----------------------------------")
 end
 
 -- deep search for a table by name
@@ -113,21 +114,90 @@ BackdoorModule.HookOutputFunctions = function()
     _G.Log = function(...) BackdoorModule.log("Trace", ...) end
     _G.LogWarn = function(...) BackdoorModule.log("Warning", ...) end
     _G.LogError = function(...) BackdoorModule.log("Error", ...) end
-    _G.print = function(...) BackdoorModule.log("Default Print", ...) end
+    _G.print = function(...) BackdoorModule.log("Print", ...) end
 end
 
 
 BackdoorModule.HookOutputFunctions()
 
-local obj, path = BackdoorModule.findObjectByName("UDPManager", _G)
+-- Function to convert a table to a string
+local function tableToString(tbl, indent)
+    local str = "{\n"
+    for k, v in pairs(tbl) do
+        local key = tostring(k)
+        local value = ""
+        if type(v) == "table" then
+            value = tableToString(v, indent .. "  ")
+        else
+            value = tostring(v)
+        end
+        str = str .. indent .. "  [" .. key .. "] = " .. value .. ",\n"
+    end
+    str = str .. indent .. "}"
+    return str
+end
+
+
+
+-- writing this at 3 am bruh
+-- quick function to hook all functions in a table
+-- this one is specific for network mgr , but later it will be adapted
+BackdoorModule.HookAllFuncs = function(tbl)
+    for k,v in pairs(tbl) do
+        if type(v) == "function" then
+            tbl[k] = FunctionHook:New(v, function(fn, ...)
+                print(k.." Arguments : "..table.concat({...}, " -> "))
+                local ret = {fn(...)}
+                local retStr = {}
+                for i, val in ipairs(ret) do
+                    if type(val) == "table" then
+                        retStr[i] = tableToString(val, "  ")
+                    else
+                        retStr[i] = tostring(val)
+                    end
+                end
+                print("Returned : ", table.concat(retStr, " ; "))
+                return table.unpack(ret)
+            end)
+        end
+    end
+end
+
+--BackdoorModule.deepdump(funnyTable)
+
+BackdoorModule.SpecialUDPManagerHook = function(tbl)
+    BackdoorModule.HookAllFuncs(tbl)
+    if tbl.Overridden then
+        print("Hooking Overriden Table")
+        BackdoorModule.HookAllFuncs(tbl.Overridden)
+    end
+end
+
+-- this is the real jackpot fr 
+local obj, path = BackdoorModule.findObjectByName("Manager.SolarNetworkManager_#1", _G)
 if obj then
     print("Object found at path: " .. table.concat(path, " -> "))
     -- Now you can modify the object as needed
-    BackdoorModule.deepdump(obj)
-    obj.Object:Delete()
+    if obj.Overridden then
+        print("Jackpot")
+        BackdoorModule.SpecialUDPManagerHook(obj)
+    end
 else
     print("Object not found.")
 end
+
+
+--[[
+-- example of hooking all functions inside a table
+funnyTable = {}
+funnyTable.TestFunction = function(x,y)
+    print("Hello "..x.." and mr "..y)
+    return 4,"Test",{["nice"] = "try"}
+end
+
+BackdoorModule.HookAllFuncs(funnyTable)
+--]]
+
 
 --[[
 GetLocalText = FunctionHook:New(GetLocalText, function(fn, ...)
